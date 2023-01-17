e96b19d117cfd5ef2b7adf692b6a3b82304a6adb
biruk-bereka <birukbeletech1212@gmail.com>
2023-01-17 13:52:40 +0300

    update index.js and navigation module

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import Navigtion from "./modules/navigation.js";
+import Navigation from './modules/navigation.js';
+import Book from './modules/book.js';
 
-Navigtion();
+Navigation();
+
+const book = new Book();
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  book.addBook();
+});
+
+book.displayBook();
